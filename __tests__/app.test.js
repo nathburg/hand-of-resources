9103ff1dea3025b87111c165cf9b3ec626fc4036
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-13 14:35:59 -0800

    C:/Program Files/Git/customers route returns all customer data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,16 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('returns all customers at /customers route', async () => {
+    const resp = await request(app).get('/customers');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(10);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,23 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS customers;
+
+create table customers (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	gender VARCHAR(50) NOT NULL
+);
+INSERT INTO customers (first_name, last_name, email, gender)
+    VALUES
+    ('Woodman', 'Habishaw', 'whabishaw0@wired.com', 'Male'),
+    ('Zorah', 'Maccraw', 'zmaccraw1@bbc.co.uk', 'Female'),
+    ('Freeman', 'Croci', 'fcroci2@businessweek.com', 'Male'),
+    ('Erna', 'Stubbert', 'estubbert3@php.net', 'Female'),
+    ('Brana', 'Buntain', 'bbuntain4@stanford.edu', 'Female'),
+    ('Bengt', 'Jansen', 'bjansen5@cloudflare.com', 'Male'),
+    ('Margalo', 'Parram', 'mparram6@chicagotribune.com', 'Female'),
+    ('Perle', 'Posvner', 'pposvner7@nifty.com', 'Female'),
+    ('Cal', 'Woollin', 'cwoollin8@cafepress.com', 'Male'),
+    ('Kale', 'Schulze', 'kschulze9@adobe.com', 'Male');
