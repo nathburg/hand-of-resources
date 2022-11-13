aa8c1f18941af3ad13c92b6388914b3495d0fe50
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-13 14:52:43 -0800

    GET /customers/:id returns just that customer's info

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,10 +7,23 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('returns all customers at /customers route', async () => {
+  it('GET /customers returns all customers', async () => {
     const resp = await request(app).get('/customers');
     expect(resp.status).toBe(200);
     expect(resp.body.length).toBe(10);
+  });
+  it('GET /customers/1 returns data for customer with id 1', async () => {
+    const resp = await request(app).get('/customers/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Object {
+        "email": "whabishaw0@wired.com",
+        "first_name": "Woodman",
+        "gender": "Male",
+        "id": 1,
+        "last_name": "Habishaw",
+      }
+    `);
   });
   afterAll(() => {
     pool.end();
